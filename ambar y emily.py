d53d9937df38a353310e0427d5dd93430d69a3e9
juaniguada1111 <bertojuanignacio89@gmail.com>
2026-06-22 11:42:45 -0300

    Add files via upload

--- ambar y emily.py
+++ ambar y emily.py
@@ -1,16 +1,29 @@
 # ===== PARTE A =====
 def nombre_valido(nombre):
     if len(nombre) >= 3 and nombre.isalpha():
-        return true
+        devolver = True
     else:
-        return false
+        devolver = False
+    return devolver
 def crear_codename(nombre, nivel):
     return nombre[0:3].upper() + "-Lv" + str(nivel)
 def vida_maxima(nivel):
     return 100 + nivel ** 2 * 5
+def main():
+    nombre = input("ingresa un nombre")
+    if nombre_valido(nombre)==True:
+         nombre = nombre.capitalize()
+    else:
+        nombre = "heroe"
+        
+    nivel = int(input("ingrese un nivel"))
+    codename = crear_codename(nombre, nivel)
+    vida = vida_maxima(nivel)
 
-    nom=input("Ingresar un usuario")
+    print("codename:", codename)
+    print("vida maxima:", vida)
     
+main()
 
 
 
